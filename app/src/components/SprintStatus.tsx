d9600d18327c47413753c4bb3956fc59939fbb87
Julien Rousseau <julrousseau20@gmail.com>
2024-01-08 23:43:13 -0500

    adjusted rendering and fixed task totals

--- app/src/components/SprintStatus.tsx
+++ app/src/components/SprintStatus.tsx
@@ -1,5 +1,12 @@
 import { Fields } from "dashboards-server";
-import { Component, createMemo, createSignal, For, Show } from "solid-js";
+import {
+    Accessor,
+    Component,
+    createMemo,
+    createSignal,
+    For,
+    Show
+} from "solid-js";
 import { sprintTasks } from "../resources/tasks";
 import { domainIcons, formatTime, tagToDomainIcon } from "../utils";
 import Dash from "./Dash";
@@ -109,7 +116,12 @@ const SprintStatus: Component<{
             </li>
           }
         >
-          {(task) => <Task task={task} subtasks={subtasks()[task.id] ?? []} />}
+          {(task) => (
+            <Task
+              task={() => task}
+              subtasks={() => subtasks()[task.id] ?? []}
+            />
+          )}
         </For>
       </ul>
     </>
@@ -117,34 +129,59 @@ const SprintStatus: Component<{
 };
 
 const Task = (props: {
-  task: TaskWithProblem;
-  subtasks: TaskWithProblem[];
+  task: Accessor<TaskWithProblem>;
+  subtasks: Accessor<TaskWithProblem[]>;
   offset?: boolean;
 }) => {
   const [showSubtasks, setShowSubtasks] = createSignal(false);
 
+  const totalTimeSpent = () =>
+    showSubtasks()
+      ? props.task().time_spent ?? 0
+      : props
+          .subtasks()
+          .reduce(
+            (total, subtask) => total + (subtask.time_spent ?? 0),
+            props.task().time_spent ?? 0,
+          );
+
+  const totalTimeEstimate = () =>
+    showSubtasks()
+      ? props.task().time_estimate ?? 0
+      : props
+          .subtasks()
+          .reduce(
+            (total, subtask) => total + (subtask.time_estimate ?? 0),
+            props.task().time_estimate ?? 0,
+          );
+
   return (
     <>
       <li
-        class="even:bg-gray-200 py-[2px]"
-        classList={{ "pl-4": props.offset }}
+        style={{ "border-color": props.task().status.color }}
+        class="relative even:bg-gray-200 py-[2px]"
+        classList={{
+          "pl-4": props.offset,
+          "after:block after:h-[110%] after:absolute after:border-inherit after:border-l-2 after:border-b-2 after:w-8 after:rounded-bl after:top-0 after:left-4 after:-translate-y-1/2":
+            props.offset,
+        }}
       >
-        <a href={props.task.url} target="_blank">
+        <a href={props.task().url} target="_blank">
           <div class="grid grid-cols-[80px_1fr_120px_70px_70px] items-center">
             <div
-              class="rounded-full w-fit px-2 mx-auto"
-              style={{ "background-color": props.task.status.color }}
+              class="rounded-full w-fit px-2 mx-auto z-10"
+              style={{ "background-color": props.task().status.color }}
             >
               <p class="uppercase font-black text-white text-center text-[0.65rem]">
-                {statusLabels[props.task.status.status]}
+                {statusLabels[props.task().status.status]}
               </p>
             </div>
 
             <div class="flex text-sm items-center">
               <For
                 each={
-                  props.task.tags.length > 0
-                    ? props.task.tags
+                  props.task().tags.length > 0
+                    ? props.task().tags
                     : [{ name: "unknown", tag_fg: "#ff80f4" }]
                 }
               >
@@ -157,12 +194,12 @@ const Task = (props: {
                   </span>
                 )}
               </For>
-              <span>{props.task.name}</span>
+              <span>{props.task().name}</span>
 
-              <Show when={props.subtasks.length > 0}>
+              <Show when={props.subtasks().length > 0}>
                 <NoPrint class="ml-auto group relative">
                   <span class="text-xs group-hover:invisible">
-                    [{props.subtasks.length}]
+                    [{props.subtasks().length}]
                   </span>
                   <button
                     class="absolute left-1/2 -translate-x-1/2 invisible group-hover:visible"
@@ -180,11 +217,13 @@ const Task = (props: {
 
             <ul class="flex flex-wrap justify-evenly w-24">
               <For
-                each={props.task.assignees.sort((a, b) =>
-                  a.username
-                    .split(" ")[1]
-                    .localeCompare(b.username.split(" ")[1]),
-                )}
+                each={props
+                  .task()
+                  .assignees.sort((a, b) =>
+                    a.username
+                      .split(" ")[1]
+                      .localeCompare(b.username.split(" ")[1]),
+                  )}
                 fallback={
                   <li>
                     <Dash />
@@ -209,29 +248,21 @@ const Task = (props: {
             </ul>
 
             <div class="h-6 text-sm text-center">
-              {props.task.time_estimate ? (
-                formatTime(props.task.time_estimate)
-              ) : (
-                <Dash />
-              )}
+              <Time time={totalTimeEstimate()} />
             </div>
 
             <div class="h-6 text-sm text-center">
-              {props.task.time_spent ? (
-                formatTime(props.task.time_spent)
-              ) : (
-                <Dash />
-              )}
+              <Time time={totalTimeSpent()} />
             </div>
           </div>
 
-          <Show when={props.task.problems.length > 0}>
+          <Show when={props.task().problems.length > 0}>
             <div class="grid grid-cols-[80px_1fr] rounded-full border-red-500 border-2 items-center">
               <span class="material-symbols-outlined text-red-500 text-center">
                 report
               </span>
               <ul class="flex flex-col">
-                <For each={props.task.problems}>
+                <For each={props.task().problems}>
                   {(problem) => (
                     <li class="text-xs">
                       <p>{problem.description}</p>
@@ -244,13 +275,19 @@ const Task = (props: {
         </a>
       </li>
 
-      <Show when={props.subtasks.length > 0 && showSubtasks()}>
-        <For each={props.subtasks}>
-          {(subtask) => <Task task={subtask} subtasks={[]} offset={true} />}
+      <Show when={props.subtasks().length > 0 && showSubtasks()}>
+        <For each={props.subtasks()}>
+          {(subtask) => (
+            <Task task={() => subtask} subtasks={() => []} offset={true} />
+          )}
         </For>
       </Show>
     </>
   );
 };
+
+function Time(props: { time: number }) {
+  return <>{formatTime(props.time) || <Dash />}</>;
+}
 
 export default SprintStatus;
