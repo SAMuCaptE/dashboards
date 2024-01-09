f40d8d1dd553789bea6592c062e796fa2e64a447
Julien Rousseau <julrousseau20@gmail.com>
2024-01-08 23:52:08 -0500

    moved to mondays instead of thursdays

--- app/src/stores/params.ts
+++ app/src/stores/params.ts
@@ -9,15 +9,27 @@ let defaultDate = window.location.search
   ?.replace("date=", "");
 
 if (defaultDate === undefined) {
-  const thursday = new Date();
-  thursday.setDate(thursday.getDate() + ((5 + 7 - thursday.getDay()) % 7));
-  defaultDate = thursday.toLocaleDateString("fr-CA");
+  // const thursday = new Date();
+  // thursday.setDate(thursday.getDate() + ((5 + 7 - thursday.getDay()) % 7));
+  // defaultDate = thursday.toLocaleDateString("fr-CA");
+
+  const monday = new Date();
+  monday.setDate(monday.getDate() + ((2 + 7 - monday.getDay()) % 7));
+  defaultDate = monday.toLocaleDateString("fr-CA");
 }
 
 const [session, setSession] = createSignal<Session>("s7");
 const [dueDate, setDueDate] = createSignal<Date>(new Date(defaultDate));
 
-const isValidDate = () => dueDate().getDay() === 4;
+// function isThursday() {
+//     return dueDate().getDay() === 4;
+// }
+
+function isMonday() {
+  return dueDate().getDay() === 1;
+}
+
+const isValidDate = isMonday;
 
 const startDate = () =>
   new Date(
