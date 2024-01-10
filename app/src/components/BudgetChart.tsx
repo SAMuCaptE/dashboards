a468416833e448c855eaffe372a0119a2012dd3e
Julien Rousseau <julrousseau20@gmail.com>
2024-01-10 16:04:28 -0500

    better budget chart rendering process

--- app/src/components/BudgetChart.tsx
+++ app/src/components/BudgetChart.tsx
@@ -8,14 +8,18 @@ const stripeColor = availableColor + "a0";
 const stripeColorFade = availableColor + "60";
 const availableStripe = `repeating-linear-gradient(-55deg, ${stripeColor}, ${stripeColor} 10px, ${stripeColorFade} 10px, ${stripeColorFade} 20px)`;
 
-const categories: Array<{ label: string; color: string }> = [
-  { label: "Boitier", color: "#ed21dc" },
-  { label: "Pièces", color: "#12a308" },
-  { label: "Données", color: "#5fdae8" },
-  { label: "Services", color: "#faaf37" },
-  { label: "Nature", color: "#dbd51a" },
-  { label: "Disponible", color: availableStripe },
-];
+type Categories =
+  | keyof Awaited<ReturnType<(typeof client)["budget"]["query"]>>["spent"]
+  | "available";
+
+const categories: Record<Categories, { label: string; color: string }> = {
+  casing: { label: "Boitier", color: "#ed21dc" },
+  composantes: { label: "Pièces", color: "#12a308" },
+  communication: { label: "Données", color: "#5fdae8" },
+  services: { label: "Services", color: "#faaf37" },
+  nature: { label: "Nature", color: "#dbd51a" },
+  available: { label: "Disponible", color: availableStripe },
+};
 
 const BudgetChart: Component = () => {
   const [budget] = createResource(() => {
@@ -27,7 +31,7 @@ const BudgetChart: Component = () => {
       {
         <div class="w-full flex items-center gap-2">
           <h4 class="font-semibold">Budget</h4>
-          <For each={categories}>
+          <For each={Object.values(categories)}>
             {(category) => (
               <Chip label={category.label} color={category.color} />
             )}
@@ -37,28 +41,48 @@ const BudgetChart: Component = () => {
 
       <div class="h-5 w-full rounded-md flex overflow-hidden mt-1 items-center">
         <Show when={budget()}>
-          <For each={[...Object.values(budget()!.spent), budget()!.available]}>
-            {(expense, index) => (
-              <div
-                class="text-center h-full flex items-center justify-center"
-                style={{
-                  background: categories[index()].color,
-                  width: Math.round((100 * expense) / budget()!.planned) + "%",
-                }}
-              >
-                <Show when={expense >= 200}>
-                  <span
-                    class="font-semibold text-white text-sm"
-                    style={{ "text-shadow": "0px 0px 3px black" }}
-                  >
-                    {Math.round(expense * 100) / 100}$
-                  </span>
-                </Show>
-              </div>
+          <For each={Object.entries(budget()!.spent)}>
+            {([category, expense]) => (
+              <Expense
+                expense={expense}
+                category={category as Categories}
+                planned={budget()!.planned}
+              />
             )}
           </For>
+
+          <Expense
+            expense={budget()!.available}
+            category={"available"}
+            planned={budget()!.planned}
+          />
         </Show>
       </div>
+    </div>
+  );
+};
+
+const Expense: Component<{
+  category: Categories;
+  expense: number;
+  planned: number;
+}> = (props) => {
+  return (
+    <div
+      class="text-center h-full flex items-center justify-center"
+      style={{
+        background: categories[props.category].color,
+        width: Math.round((100 * props.expense) / props.planned) + "%",
+      }}
+    >
+      <Show when={props.expense >= 200}>
+        <span
+          class="font-semibold text-white text-sm"
+          style={{ "text-shadow": "0px 0px 3px black" }}
+        >
+          {Math.round(props.expense * 100) / 100}$
+        </span>
+      </Show>
     </div>
   );
 };
