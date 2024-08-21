e012b5618be3822922d17a202ef99828fa6da2e3
Julien Rousseau <julrousseau20@gmail.com>
2024-08-21 16:27:24 -0400

    pretty much completed ui

--- app/src/components/TimeLogger.tsx
+++ app/src/components/TimeLogger.tsx
@@ -40,14 +40,25 @@ const TimeLogger: Component = () => {
   );
   const [taskDetails, setTaskDetails] = createSignal<Task | null>(null);
 
+  const [saving, setSaving] = createSignal(false);
+
   const handleUserSelect = (ev: Event & { target: HTMLSelectElement }) => {
+    if (timerIsActive()) {
+      return;
+    }
     localStorage.setItem(USER_ID, ev.target.value);
     setSelectedUserId(ev.target.value);
   };
 
   const handleSubmit = (ev: Event) => {
     ev.preventDefault();
+    if (timerIsActive()) {
+      return;
+    }
+
     console.log("submitted time entry");
+    setSaving(true);
+    setTimeout(() => setSaving(false), 5000);
   };
 
   const handleManualTimer = () => {
@@ -95,151 +106,187 @@ const TimeLogger: Component = () => {
     <NoPrint>
       <Show when={(users() ?? []).length > 0} fallback={<Loader />}>
         <div class="border-black border-2 rounded-lg p-4 m-2">
-          <form ref={(el) => (formRef = el)} onSubmit={handleSubmit}>
-            <div class="flex justify-center gap-4">
-              <div>
+          <Show when={saving() === false} fallback={<Loader />}>
+            <form ref={(el) => (formRef = el)} onSubmit={handleSubmit}>
+              <div class="flex justify-center gap-4">
                 <div>
-                  <label for={USER_ID} class="pr-2">
-                    <i>SAUM</i>atelot:
-                  </label>
-                  <select
-                    id={USER_ID}
-                    name={USER_ID}
-                    value={selectedUserId()}
-                    onChange={handleUserSelect}
-                    class="border-[1px] bg-white px-2 rounded"
-                  >
-                    <option value="">Aucune sélection</option>
-                    {users()?.map((user) => (
-                      <option value={user.id}>{user.username}</option>
-                    ))}
-                  </select>
-                </div>
-                <div>
-                  <div class="flex">
-                    <label for="task" class="pr-2">
-                      Tâche:
+                  <div>
+                    <label for={USER_ID} class="pr-2 w-24">
+                      <i>SAUM</i>atelot:
                     </label>
-                    <input
-                      id="task"
-                      type="search"
-                      autocomplete="off"
-                      value={taskInput()}
-                      onChange={(ev) => setTaskInput(ev.target.value)}
-                      class="border-[1px] bg-white px-2 rounded text-sm flex-1"
-                    />
-                  </div>
-                  <div class="border-[1px] bg-slate-50 rounded-lg p-2 my-2">
-                    <Show
-                      when={taskDetails()}
-                      fallback={
-                        <div class="flex justify-center min-h-12">
-                          <div class="block relative w-12 aspect-square">
-                            <img
-                              src={import.meta.env.BASE_URL + "clickup.svg"}
-                            />
-                          </div>
-                        </div>
-                      }
+                    <select
+                      id={USER_ID}
+                      name={USER_ID}
+                      value={selectedUserId()}
+                      disabled={timerIsActive()}
+                      onChange={handleUserSelect}
+                      class="border-[1px] bg-white px-2 rounded disabled:bg-gray-200"
                     >
-                      <a href={taskDetails()!.url} target="_blank">
-                        <div class="flex gap-2 items-center">
-                          <div class="block relative w-5 aspect-square">
-                            <img
-                              src={import.meta.env.BASE_URL + "clickup.svg"}
-                            />
-                          </div>
-                          <div>
-                            <p class="font-medium text-sm">
-                              {taskDetails()!.name}
-                            </p>
-                            <div class="flex gap-1">
-                              <For each={taskDetails()!.assignees}>
-                                {(assignee) => (
-                                  <div
-                                    class="w-6 h-6 rounded-full block object-contain"
-                                    style={{
-                                      "background-color": assignee.color,
-                                    }}
-                                  >
-                                    <p class="font-semibold text-white text-center text-[0.65rem] leading-6 align-middle">
-                                      {assignee.initials}
-                                    </p>
-                                  </div>
-                                )}
-                              </For>
-                              <For each={taskDetails()!.tags}>
-                                {(tag) => (
-                                  <div
-                                    style={{
-                                      background:
-                                        colors[
-                                          tag.name as keyof typeof colors
-                                        ] || colors.unknown,
-                                    }}
-                                    class="rounded-full flex items-center px-2 w-fit"
-                                  >
-                                    <span class="material-symbols-outlined pr-1 font-bold text-xs text-white">
-                                      {tagToDomainIcon(tag) ||
-                                        domainIcons.unknown}
-                                    </span>
-                                    <span class="text-white font-medium text-xs">
-                                      {tag.name}
-                                    </span>
-                                  </div>
-                                )}
-                              </For>
+                      <option value="">Aucune sélection</option>
+                      {users()?.map((user) => (
+                        <option value={user.id}>{user.username}</option>
+                      ))}
+                    </select>
+                  </div>
+                  <div>
+                    <div class="flex pt-1">
+                      <label for="task" class="pr-2 w-24">
+                        Tâche:
+                      </label>
+                      <input
+                        id="task"
+                        type="search"
+                        autocomplete="off"
+                        value={taskInput()}
+                        disabled={timerIsActive()}
+                        onChange={(ev) =>
+                          setTaskInput((previous) =>
+                            timerIsActive() ? previous : ev.target.value,
+                          )
+                        }
+                        class="border-[1px] bg-white px-2 rounded text-sm flex-1 disabled:bg-gray-200"
+                      />
+                    </div>
+                    <div class="border-[1px] bg-slate-50 rounded-lg p-2 my-2">
+                      <Show
+                        when={taskDetails()}
+                        fallback={
+                          <div class="flex justify-center min-h-12">
+                            <div class="block relative w-12 aspect-square">
+                              <img
+                                src={import.meta.env.BASE_URL + "clickup.svg"}
+                              />
                             </div>
                           </div>
-                        </div>
-                      </a>
-                    </Show>
+                        }
+                      >
+                        <a href={taskDetails()!.url} target="_blank">
+                          <div class="flex gap-2 items-center">
+                            <div class="block relative w-5 aspect-square">
+                              <img
+                                src={import.meta.env.BASE_URL + "clickup.svg"}
+                              />
+                            </div>
+                            <div>
+                              <p class="font-medium text-sm">
+                                {taskDetails()!.name}
+                              </p>
+                              <div class="flex gap-1">
+                                <For each={taskDetails()!.assignees}>
+                                  {(assignee) => (
+                                    <div
+                                      class="w-6 h-6 rounded-full block object-contain"
+                                      style={{
+                                        "background-color": assignee.color,
+                                      }}
+                                    >
+                                      <p class="font-semibold text-white text-center text-[0.65rem] leading-6 align-middle">
+                                        {assignee.initials}
+                                      </p>
+                                    </div>
+                                  )}
+                                </For>
+                                <For each={taskDetails()!.tags}>
+                                  {(tag) => (
+                                    <div
+                                      style={{
+                                        background:
+                                          colors[
+                                            tag.name as keyof typeof colors
+                                          ] || colors.unknown,
+                                      }}
+                                      class="rounded-full flex items-center px-2 w-fit"
+                                    >
+                                      <span class="material-symbols-outlined pr-1 font-bold text-xs text-white">
+                                        {tagToDomainIcon(tag) ||
+                                          domainIcons.unknown}
+                                      </span>
+                                      <span class="text-white font-medium text-xs">
+                                        {tag.name}
+                                      </span>
+                                    </div>
+                                  )}
+                                </For>
+                              </div>
+                            </div>
+                          </div>
+                        </a>
+                      </Show>
+                    </div>
+                  </div>
+                </div>
+
+                <div>
+                  <div class="flex">
+                    <label for="start-datetime" class="pr-2 w-16">
+                      Début:
+                    </label>
+                    <input
+                      id="start-datetime"
+                      type="datetime-local"
+                      disabled={timerIsActive()}
+                      value={convertToDateTimeLocalString(startTime())}
+                      class="border-[1px] bg-white px-2 rounded flex-1 text-sm disabled:bg-gray-200"
+                      onChange={(ev) =>
+                        setStartTime((previous) =>
+                          timerIsActive()
+                            ? previous
+                            : new Date(ev.target.value),
+                        )
+                      }
+                    />
+                  </div>
+                  <div class="flex pt-1">
+                    <label for="end-datetime" class="pr-2 w-16">
+                      Fin:
+                    </label>
+                    <input
+                      id="end-datetime"
+                      type="datetime-local"
+                      disabled={timerIsActive()}
+                      value={convertToDateTimeLocalString(endTime())}
+                      class="border-[1px] bg-white px-2 rounded flex-1 text-sm disabled:bg-gray-200"
+                      onChange={(ev) =>
+                        setEndTime((previous) =>
+                          timerIsActive()
+                            ? previous
+                            : new Date(ev.target.value),
+                        )
+                      }
+                    />
+                  </div>
+                  <div class="flex">
+                    <p class="pr-2 w-16">Durée:</p>
+                    <p>{duration() || "-"}</p>
+                  </div>
+
+                  <div>
+                    <button
+                      type="button"
+                      onClick={handleManualTimer}
+                      class="flex items-center border-[1px] px-2 py-1 bg-gray-100 rounded-md mt-3 mx-auto"
+                    >
+                      <span class="material-symbols-outlined pr-1">timer</span>
+                      <p class="text-sm">
+                        {timerIsActive()
+                          ? "Arrêter le minuteur"
+                          : "Démarrer le minuteur"}
+                      </p>
+                    </button>
                   </div>
                 </div>
               </div>
 
-              <div>
-                <div class="flex">
-                  <label for="start-datetime" class="pr-2">
-                    Début:
-                  </label>
-                  <input
-                    id="start-datetime"
-                    type="datetime-local"
-                    value={convertToDateTimeLocalString(startTime())}
-                    class="border-[1px] bg-white px-2 rounded flex-1"
-                    onChange={(ev) => setStartTime(new Date(ev.target.value))}
-                  />
-                </div>
-                <div class="flex">
-                  <label for="end-datetime" class="pr-2">
-                    Fin:
-                  </label>
-                  <input
-                    id="end-datetime"
-                    type="datetime-local"
-                    value={convertToDateTimeLocalString(endTime())}
-                    class="border-[1px] bg-white px-2 rounded flex-1"
-                    onChange={(ev) => setEndTime(new Date(ev.target.value))}
-                  />
-                </div>
-                <div class="flex">
-                  <p class="pr-2">Durée:</p>
-                  <p>{duration() || "-"}</p>
-                </div>
-
-                <div>
-                  <button onClick={handleManualTimer} type="button">
-                    {timerIsActive()
-                      ? "Arrêter le minuteur"
-                      : "Démarrer le minuteur"}
-                  </button>
-                </div>
-              </div>
-            </div>
-
-            <button type="submit">Enregistrer</button>
-          </form>
+              <button
+                type="submit"
+                disabled={timerIsActive()}
+                class="flex items-center mx-auto mt-2 px-2 py-1 bg-gray-100 rounded-md"
+              >
+                <span class="material-symbols-outlined pr-1">bookmark</span>
+                <p class="text-sm">Enregistrer</p>
+              </button>
+            </form>
+          </Show>
         </div>
       </Show>
     </NoPrint>
